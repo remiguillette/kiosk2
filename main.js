af9075cd629f97dfcaa748e0014c2ba5295f5178
Rémi Guillette <remiguillette@gmail.com>
2025-10-02 01:07:29 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    kiosk: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  });
+
+  // Démarrer sur le menu local
+  win.loadFile('menu.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
